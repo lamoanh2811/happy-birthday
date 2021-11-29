87131e7445bacbfd0928aaa53f48d9cbea3fd295
lamoanh2811 <95197649+lamoanh2811@users.noreply.github.com>
2021-11-29 09:15:36 +0700

    Add files via upload

---
+++ js/letter.js
@@ -0,0 +1,73 @@
+
+var aparted = false;
+
+$("#open").click(function(){
+	
+	if(!aparted)
+	{
+		var typed = new Typed('.letter', {
+			strings: ["^1000Gửi Lâm Quanh", 
+				"Hi bạn,<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chắc cũng được 5 năm từ lúc biết nhau rồi. Mình không biết bạn đang trông chờ gì ở đứa ngu văn 12 năm liền như mình. Nhưng mà sinh nhật 21 vui vẻ nha bạn.<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;P/s: Nhớ học tiếng anh nha bạn!<br><br><p style='float:right; display:block;'>Happy birthday Lam Quanh!</p><br><p style='float:right; display:block;'>28/11/2021</p>&nbsp;&nbsp;&nbsp;&nbsp;"],
+			typeSpeed: 50,
+			backSpeed: 50
+		});
+		
+		$('#open').find("span").eq(0).css('background-position', "0 -150px");
+		
+		aparted = true;
+		
+		var music = document.getElementById('music2');		
+		if (music.paused)
+		{
+			music.play();
+			$('#music_btn2').css("opacity", "1"); 
+		}
+	}
+	
+});
+
+function playPause() 
+{
+    var music = document.getElementById('music2');
+    var music_btn = $('#music_btn2');
+	
+    if (music.paused)
+	{
+        music.play();
+		music_btn.css("opacity", "1"); 
+    }
+    else
+	{
+        music.pause();
+		music_btn.css("opacity", "0.2"); 
+    }	
+}
+
+
+window.onload = function () 
+{	
+
+	var currentUrl = window.location.href;
+	var firstIndex = currentUrl.indexOf("#");	
+	if (firstIndex <= 0) window.location.href = currentUrl + "#contact";
+
+	$('#music2').attr('src', bgmsrc);
+
+	document.addEventListener('touchstart',function (event) { if(event.touches.length > 1) event.preventDefault(); });
+	
+	var lastTouchEnd = 0;
+	
+	document.addEventListener('touchend',function (event) {
+		
+		var now = (new Date()).getTime();
+		if(now - lastTouchEnd <= 300) event.preventDefault();
+		lastTouchEnd = now;
+		
+	}, false);
+	
+	document.addEventListener('gesturestart', function (event) { event.preventDefault(); });
+
+	$('body').css('opacity', '1');
+	$('#jsi-cherry-container').css('z-index', '-99');
+	
+}
